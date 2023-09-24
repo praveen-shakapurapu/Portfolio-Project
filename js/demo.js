cf441ebde0a741de9f13e3f36a642a2ffe509af4
Praveen <praveenshakapurapu@gmail.com>
2023-09-24 18:54:33 +0530

    second commit

---
+++ js/demo.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector("#menubar")
+let navbar = document.querySelector(".navbar")
+
+menu.addEventListener("click", () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+})
+
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+
+function blurFunc(){
+    let parent = this.parentNode;
+    parent.classList.remove("focus");
+}
+
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value == ""){
+    parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc)
+    input.addEventListener("blur", blurFunc)
+});
+
+
+// data = "This is java script"
+// typeof(data)
